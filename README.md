20ed51a0aa2a34a5098da72faac7055ff64017e9
ebukaracer <52864306+ebukaracer@users.noreply.github.com>
2025-01-24 11:42:20 +0100

    Initial release
    ## Changes
    
    Initial release for v1.0.0

---
+++ README.md
@@ -0,0 +1,49 @@
+# EzPooler
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-blue)](http://makeapullrequest.com) [![License: MIT](https://img.shields.io/badge/License-MIT-blue)](https://ebukaracer.github.io/ebukaracer/md/LICENSE.html)
+
+**EzPooler** is a Unity package that provides an efficient way to manage object pooling in your game.
+
+ [Read Docs](https://ebukaracer.github.io/EzPooler)
+ 
+## Features  
+- Efficient object pooling to improve performance.  
+- Easy-to-use methods for spawning and despawning gameobjects.  
+- Support for pre-instantiating and caching gameobjects in the Unity Editor for efficient use during gameplay.
+
+## Installation
+ *In unity editor inside package manager:*
+- Hit `(+)`, choose `Add package from Git URL`(Unity 2019.4+)
+- Paste the `URL` for this package inside the box: https://github.com/ebukaracer/EzPooler.git#upm
+- Hit `Add`
+- If you're using assembly definition in your project, be sure to add this package's reference under: `Assembly Definition References` or check out [this](https://ebukaracer.github.io/ebukaracer/md/SETUPGUIDE.html)
+
+## Quick Usage
+```csharp
+using Racer.EzPooler.Core;
+using UnityEngine;
+
+public class ExampleUsage : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    private PoolManager _poolManager;
+
+    private void Start()
+    {
+        _poolManager = GetComponent<PoolManager>();
+
+        // Spawn an object
+        var spawnedObject = _poolManager.SpawnObject(Vector3.zero, Quaternion.identity);
+
+        // Despawn the object after 2 seconds
+        spawnedObject.InvokeDespawn(2f);
+    }
+}
+```
+
+## Samples and Best Practices
+Check out this package's sample scene by importing it from the package manager *sample's tab* and exploring the scripts for the recommended approach for pooling gameobjects in your game.
+
+*To remove this package completely(leaving no trace), navigate to: `Racer > EzPooler > Remove package`*
+
+## [Contributing](https://ebukaracer.github.io/ebukaracer/md/CONTRIBUTING.html) 
+Contributions are welcome! Please open an issue or submit a pull request.
---
+++ src/Runtime/Core/PoolManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using Racer.EzPooler.Utilities;
+using UnityEngine;
+
+namespace Racer.EzPooler.Core
+{
+    /// <summary>
+    /// Manages a pool of reusable objects to optimize performance by reducing the need for frequent instantiation and destruction.
+    /// </summary>
+    [DefaultExecutionOrder(-100)]
+    public class PoolManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Queue to hold the pool objects.
+        /// </summary>
+        private readonly Queue<PoolObject> _queue = new();
+
+        /// <summary>
+        /// Current count of objects in the pool.
+        /// </summary>
+        private int _currentObjectsCount;
+
+        /// <summary>
+        /// Initial capacity of the pool.
+        /// </summary>
+        [SerializeField] private int capacity = 5;
+
+        /// <summary>
+        /// Flag to determine if the pool should automatically grow when capacity is reached.
+        /// </summary>
+        [SerializeField] private bool autoGrow;
+
+        /// <summary>
+        /// Prefab of the pool object to be instantiated.
+        /// </summary>
+        [SerializeField] private PoolObject poolObjectPrefab;
+
+        /// <summary>
+        /// Queue to hold the pool objects for the editor.
+        /// </summary>
+        [field: SerializeField]
+        internal List<PoolObject> CachedObjects { get; private set; } = new();
+
+
+        private void Awake()
+        {
+            // Assign the objects in editor list to the runtime queue.
+            foreach (var poolObj in CachedObjects)
+            {
+                poolObj.PoolManager = this;
+                _queue.Enqueue(poolObj);
+            }
+        }
+
+        private void AutoGrowQueue()
+        {
+            var poolObj = Instantiate(poolObjectPrefab, transform);
+
+            if (!poolObj)
+            {
+                EzLogger.Warn(
+                    $"[{poolObj}] does not contain [{nameof(PoolObject)} Component], either add or inherit from it.",
+                    poolObj);
+                return;
+            }
+
+            poolObj.PoolManager = this;
+
+            if (poolObj.gameObject.activeInHierarchy)
+                poolObj.gameObject.SetActive(false);
+
+            _queue.Enqueue(poolObj);
+        }
+
+        /// <summary>
+        /// Spawns an object from the pool at the prefab's position and rotation.
+        /// </summary>
+        /// <returns>The spawned pool object.</returns>
+        public PoolObject SpawnObject()
+        {
+            Transform objTransform;
+            return SpawnObject((objTransform = poolObjectPrefab.transform).position, objTransform.rotation);
+        }
+
+        /// <summary>
+        /// Spawns an object from the pool at the specified position.
+        /// </summary>
+        /// <param name="position">The position to spawn the object at.</param>
+        /// <returns>The spawned pool object.</returns>
+        public PoolObject SpawnObject(Vector3 position) => SpawnObject(position, poolObjectPrefab.transform.rotation);
+
+        /// <summary>
+        /// Spawns an object from the pool at the specified position and rotation.
+        /// </summary>
+        /// <param name="position">The position to spawn the object at.</param>
+        /// <param name="rotation">The rotation to spawn the object with.</param>
+        /// <returns>The spawned pool object.</returns>
+        public PoolObject SpawnObject(Vector3 position, Quaternion rotation)
+        {
+            if (_queue.Count <= 0)
+            {
+                if (autoGrow && _currentObjectsCount >= capacity)
+                {
+                    AutoGrowQueue();
+                    capacity++;
+                }
+                else if (_currentObjectsCount < capacity)
+                {
+                    AutoGrowQueue();
+                }
+                else
+                {
+                    EzLogger.Error(
+                        $"Pool out of objects, no more game objects available in the [{name}] pool.\n"
+                        + "Make sure to increase the [Capacity] or tick the [Auto Grow] check-box in the inspector.\n\n",
+                        this);
+
+                    return null;
+                }
+            }
+
+            var poolObj = _queue.Dequeue();
+
+            poolObj.transform.SetPositionAndRotation(position, rotation);
+            poolObj.gameObject.SetActive(true);
+            _currentObjectsCount++;
+
+            return poolObj;
+        }
+
+        /// <summary>
+        /// Despawns an object, returning it to the pool.
+        /// </summary>
+        /// <param name="poolObj">The pool object to despawn.</param>
+        public void DespawnObject(PoolObject poolObj)
+        {
+            if (!poolObj.gameObject.activeInHierarchy)
+                return;
+
+            poolObj.gameObject.SetActive(false);
+
+            _queue.Enqueue(poolObj);
+        }
+
+        /// <summary>
+        /// Despawns an object after a delay, returning it to the pool.
+        /// </summary>
+        /// <param name="poolObj">The pool object to despawn.</param>
+        /// <param name="delay">The delay in seconds before despawning the object.</param>
+        public void DespawnObject(PoolObject poolObj, float delay)
+        {
+            poolObj.InvokeDespawn(delay);
+        }
+    }
+}
